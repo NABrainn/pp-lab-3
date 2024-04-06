aa4cffee9c5459e9ff4b487aa069fe0285a431ec
nabrain <michal2002.60@gmail.com>
2024-04-06 17:48:59 +0200

    zad1

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Point point = new Point(1, 2);
+        System.out.println("X: " + point.getX());
+        System.out.println("Y: " + point.getY());
+    }
+}

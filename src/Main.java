23a10a233109879c1432dab56c61852a5c105f45
nabrain <michal2002.60@gmail.com>
2024-04-06 18:58:30 +0200

    zad6.1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import geometry.Square;
 public class Main {
     public static void main(String[] args) {
         Square square = new Square(6);
+        System.out.println("Area: " + square.calculateArea() + " Perimeter: " + square.calculatePerimeter());
     }
 }

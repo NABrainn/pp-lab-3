db3368ad103a453940fb0eea4f45eb93ac3160da
nabrain <michal2002.60@gmail.com>
2024-04-06 19:00:25 +0200

    zad5.1

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ import geometry.Rectangle;
 public class Main {
     public static void main(String[] args) {
         Rectangle rectangle = new Rectangle(5, 7);
+        System.out.println("Area: " + rectangle.calculateArea() + " Perimeter: " + rectangle.calculatePerimeter());
     }
 }

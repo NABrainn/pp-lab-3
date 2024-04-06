7bc24be50d9f1c4b9c4279297583e1578f0a2bf6
nabrain <michal2002.60@gmail.com>
2024-04-06 19:13:40 +0200

    zad2.1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Point point = new Point(2, 3);
         Circle circle = new Circle(point, 4);
-        System.out.println("Center: " + circle.getCenter() + " Radius: " + circle.getRadius());
+        System.out.println("Center: " + circle.getCenter().getX() + ", " + circle.getCenter().getY() + " Radius: " + circle.getRadius());
     }
 }

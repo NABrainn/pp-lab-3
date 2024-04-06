d585489b3554b79ecb1d13c1ce8184fbca094c7c
nabrain <michal2002.60@gmail.com>
2024-04-06 18:05:58 +0200

    zad2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import geometry.Point;
+import geometry.Circle;
+
 public class Main {
     public static void main(String[] args) {
-        Point point = new Point(1, 2);
-        System.out.println("X: " + point.getX());
-        System.out.println("Y: " + point.getY());
+        Point point = new Point(2, 3);
+        Circle circle = new Circle(point, 4);
+        System.out.println("Center: " + circle.getCenter() + " Radius: " + circle.getRadius());
     }
 }

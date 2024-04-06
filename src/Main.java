f1797456f1271f1350fd7faa68f17a26e36d5337
nabrain <michal2002.60@gmail.com>
2024-04-06 19:12:34 +0200

    zad3.1

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         Point point = new Point(6.4, 8);
         ColoredCircle coloredCircle = new ColoredCircle(point, 10, "blue");
-        System.out.println("Center: " + coloredCircle.getCenter() + " Radius: " + coloredCircle.getRadius() + " Color: " + coloredCircle.getColor());
+        System.out.println("Center: " + coloredCircle.getCenter().getX() + ", " + coloredCircle.getCenter().getY() + " Radius: " + coloredCircle.getRadius() + " Color: " + coloredCircle.getColor());
     }
 }
